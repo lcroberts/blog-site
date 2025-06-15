6cf934d20f5a1b86adc34f6f6687cddd41045612
Logan Roberts <logan@lcroberts.dev>
2025-06-15 14:12:15 +0000

    Get mobile responsive nav bar working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,53 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
 document.documentElement.classList.toggle(
   "dark",
   localStorage.theme === "dark" ||
     (!("theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches),
 );
-// Whenever the user explicitly chooses light mode
-// localStorage.theme = "light";
-// Whenever the user explicitly chooses dark mode
-// localStorage.theme = "dark";
-// Whenever the user explicitly chooses to respect the OS preference
-// localStorage.removeItem("theme");
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#copyright-year").innerHTML =
-    new Date().getFullYear();
+function handleNavbar() {
+  const mobileMenuButton = document.querySelector(".mobile-menu-button");
+  const mobileMenu = document.querySelector(".navigation-menu");
 
+  mobileMenuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  // Select all dropdown toggle buttons
+  const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
+
+  dropdownToggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      // Find the next sibling element which is the dropdown menu
+      const dropdownMenu = toggle.nextElementSibling;
+
+      // Toggle the 'hidden' class to show or hide the dropdown menu
+      if (dropdownMenu.classList.contains("hidden")) {
+        // Hide any open dropdown menus before showing the new one
+        document.querySelectorAll(".dropdown-menu").forEach((menu) => {
+          menu.classList.add("hidden");
+        });
+
+        dropdownMenu.classList.remove("hidden");
+      } else {
+        dropdownMenu.classList.add("hidden");
+      }
+    });
+  });
+
+  // Optional: Clicking outside of an open dropdown menu closes it
+  window.addEventListener("click", (event) => {
+    if (!event.target.matches(".dropdown-toggle")) {
+      document.querySelectorAll(".dropdown-menu").forEach((menu) => {
+        if (!menu.contains(event.target)) {
+          menu.classList.add("hidden");
+        }
+      });
+    }
+  });
+}
+
+function handleLightSwitch() {
   const lightSwitches = document.querySelectorAll(".light-switch");
   if (lightSwitches.length > 0) {
     lightSwitches.forEach((lightSwitch, i) => {
@@ -39,4 +71,11 @@ document.addEventListener("DOMContentLoaded", () => {
       };
     });
   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#copyright-year").innerHTML =
+    new Date().getFullYear();
+  handleLightSwitch();
+  handleNavbar();
 });
