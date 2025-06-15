f511fc57f0085dcfa488c60b5f9ff73fed1edb07
Logan Roberts <logan@lcroberts.dev>
2025-06-15 23:06:44 +0000

    Add mobile navbar animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,40 +10,7 @@ function handleNavbar() {
   const mobileMenu = document.querySelector(".navigation-menu");
 
   mobileMenuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
-
-  // Select all dropdown toggle buttons
-  const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
-
-  dropdownToggles.forEach((toggle) => {
-    toggle.addEventListener("click", () => {
-      // Find the next sibling element which is the dropdown menu
-      const dropdownMenu = toggle.nextElementSibling;
-
-      // Toggle the 'hidden' class to show or hide the dropdown menu
-      if (dropdownMenu.classList.contains("hidden")) {
-        // Hide any open dropdown menus before showing the new one
-        document.querySelectorAll(".dropdown-menu").forEach((menu) => {
-          menu.classList.add("hidden");
-        });
-
-        dropdownMenu.classList.remove("hidden");
-      } else {
-        dropdownMenu.classList.add("hidden");
-      }
-    });
-  });
-
-  // Optional: Clicking outside of an open dropdown menu closes it
-  window.addEventListener("click", (event) => {
-    if (!event.target.matches(".dropdown-toggle")) {
-      document.querySelectorAll(".dropdown-menu").forEach((menu) => {
-        if (!menu.contains(event.target)) {
-          menu.classList.add("hidden");
-        }
-      });
-    }
+    mobileMenu.classList.toggle("max-h-24");
   });
 }
 
